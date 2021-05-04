58b9343a5fa0aa0df2534b2eb811c4db3d8177a1
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2021-05-03 21:56:48 -0300

    Páginda de teste de conexão. Teste.aspx

---
+++ Asp.NetBD1/Asp.NetBD1/Teste.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Teste.aspx.cs" Inherits="Asp.NetBD1.Teste" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="text-center">
+        <asp:Button ID="btnTestar" runat="server" CssClass="btn btn-primary"
+            Text="Testar Conexão BD" OnClick="btnTestar_Click" />
+        <br />
+        <asp:Label ID="lblResultado" runat="server" CssClass="text-capitalize"></asp:Label>
+    </div>
+</asp:Content>
---
+++ Asp.NetBD1/Asp.NetBD1/Teste.aspx.cs
@@ -0,0 +1,40 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetBD1
+{
+    public partial class Teste : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTestar_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            try
+            {
+                cmd.Connection = Conexao.Connection;
+                Conexao.Conectar();
+                lblResultado.CssClass = "text text-success";
+                lblResultado.Text = "Conectado com Sucesso";
+            }
+            catch (Exception ex)
+            {
+                lblResultado.CssClass = "text text-danger";
+                lblResultado.Text = $"Atenção: {ex.Message}";
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+
+        }
+    }
+}
