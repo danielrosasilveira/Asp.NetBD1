eb4a4ccf071f97e3a7422ec9d138c81482d5e574
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2021-05-10 22:10:25 -0300

    10/05/2021 - Web.config e Inserir Cliente

---
+++ Asp.NetBD1/Asp.NetBD1/Inserir.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetBD1
+{
+    public partial class Inserir1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGravar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "Olá";
+        }
+    }
+}
---
+++ Asp.NetBD1/Asp.NetBD1/old_Inserir.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="old_Inserir.aspx.cs" Inherits="Asp.NetBD1.Inserir" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+
+</asp:Content>
