866a8339d9f171a344d0d6b3cf7d68984e638ae8
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2021-05-17 22:00:13 -0300

    Editar Clientes

---
+++ Asp.NetBD1/Asp.NetBD1/Editar.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Editar.aspx.cs" Inherits="Asp.NetBD1.Editar" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="text-center text-primary">
+        <h2>Edita Dados do Cliente</h2>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="col-md-2">
+            <label>ID:</label>
+            <asp:TextBox ID="txtID" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+        <div class="col-md-10">
+            <label>Nome:</label>
+            <asp:TextBox ID="txtNome" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="col-md-7">
+            <label>Logradouro:</label>
+            <asp:TextBox ID="txtLogradouro" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div class="col-md-2">
+            <label>Número:</label>
+            <asp:TextBox ID="txtNumero" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div class="col-md-3">
+            <label>Complemento:</label>
+            <asp:TextBox ID="txtComplemento" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="col-md-5">
+            <label>Bairro:</label>
+            <asp:TextBox ID="txtBairro" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div class="col-md-5">
+            <label>Cidade:</label>
+            <asp:TextBox ID="txtCidade" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+        <div class="col-md-2">
+            <label>UF:</label>
+            <asp:TextBox ID="txtUF" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="text-right col-md-12">
+            <asp:Button ID="btnAlterar" CssClass="btn btn-warning" runat="server" Text="Alterar" OnClick="btnAlterar_Click"/>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="text-rigth col-md-12">
+            <asp:Label ID="lblResultado" CssClass="text-sucess" runat="server"></asp:Label>
+        </div>
+    </div>
+</asp:Content>
---
+++ Asp.NetBD1/Asp.NetBD1/Editar.aspx.cs
@@ -0,0 +1,127 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetBD1
+{
+    public partial class Editar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (CapturaID())
+                {
+                    DadosConsulta();
+                }
+            }
+        }
+
+        #region CapturaID
+        private bool CapturaID()
+        {
+            return Request.QueryString.AllKeys.Contains("id");
+        }
+        #endregion
+
+        #region DadosConsulta
+        private void DadosConsulta()
+        {
+            int IDCliente = ObterIDCliente();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao.Connection;
+                cmd.CommandText = @"select * from cliente
+                                    where cli_id = @IDCliente";
+
+                cmd.Parameters.AddWithValue("@IDCliente", IDCliente);
+
+                Conexao.Conectar();
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    txtID.Text = reader["cli_id"].ToString();
+                    txtNome.Text = reader["cli_nome"].ToString();
+                    txtLogradouro.Text = reader["cli_logradouro"].ToString();
+                    txtNumero.Text = reader["cli_numero"].ToString();
+                    txtComplemento.Text = reader["cli_complemento"].ToString();
+                    txtBairro.Text = reader["cli_bairro"].ToString();
+                    txtCidade.Text = reader["cli_cidade"].ToString();
+                    txtUF.Text = reader["cli_uf"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+        }
+        #endregion
+
+        #region ObterIDCliente
+        private int ObterIDCliente()
+        {
+            int id = 0;
+            var idURL = Request.QueryString["id"];
+            if (!int.TryParse(idURL, out id))
+            {
+                throw new Exception("ID Inválido");
+            }
+            if (id <= 0)
+            {
+                throw new Exception("ID Inválido");
+            }
+            return id;
+        }
+        #endregion
+
+        #region Botão Alterar
+        protected void btnAlterar_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            try
+            {
+                cmd.Connection = Conexao.Connection;
+                cmd.CommandText = @"update cliente
+                                    set cli_nome    = @nome,
+                                    cli_logradouro  = @logradouro,
+                                    cli_numero      = @numero,
+                                    cli_complemento = @complemento,
+                                    cli_bairro      = @bairro,
+                                    cli_cidade      = @cidade,
+                                    cli_uf          = @uf
+                                    where cli_id    = @id";
+                cmd.Parameters.AddWithValue("@id", txtID.Text);
+                cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@logradouro", txtLogradouro.Text);
+                cmd.Parameters.AddWithValue("@numero", txtNumero.Text);
+                cmd.Parameters.AddWithValue("@complemento", txtComplemento.Text);
+                cmd.Parameters.AddWithValue("@bairro", txtBairro.Text);
+                cmd.Parameters.AddWithValue("@cidade", txtCidade.Text);
+                cmd.Parameters.AddWithValue("@uf", txtUF.Text);
+
+                Conexao.Conectar();
+                cmd.ExecuteNonQuery();
+                Response.Redirect("Listar.aspx");
+            }
+            catch (Exception ex)
+            {
+                lblResultado.Text = $"Error: {ex.Message}";
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+        }
+        #endregion
+    }
+}
