e39dfe8a82abac22e193751c039863b685baaef5
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2021-05-17 21:11:45 -0300

    Detalhes Cliente

---
+++ Asp.NetBD1/Asp.NetBD1/Detalhes.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Detalhes.aspx.cs" Inherits="Asp.NetBD1.Detalhes" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="text-center text-primary">
+        <h2>Consulta Dados do Cliente</h2>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="col-md-2">
+            <label>ID:</label>
+            <asp:TextBox ID="txtID" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+        <div class="col-md-10">
+            <label>Nome:</label>
+            <asp:TextBox ID="txtNome" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="col-md-7">
+            <label>Logradouro:</label>
+            <asp:TextBox ID="txtLogradouro" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+        <div class="col-md-2">
+            <label>Número:</label>
+            <asp:TextBox ID="txtNumero" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+        <div class="col-md-3">
+            <label>Complemento:</label>
+            <asp:TextBox ID="txtComplemento" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top: 15px">
+        <div class="col-md-5">
+            <label>Bairro:</label>
+            <asp:TextBox ID="txtBairro" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+        <div class="col-md-5">
+            <label>Cidade:</label>
+            <asp:TextBox ID="txtCidade" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+        <div class="col-md-2">
+            <label>UF:</label>
+            <asp:TextBox ID="txtUF" runat="server" CssClass="form-control"
+                Enabled="false"></asp:TextBox>
+        </div>
+    </div>
+</asp:Content>
---
+++ Asp.NetBD1/Asp.NetBD1/Detalhes.aspx.cs
@@ -0,0 +1,86 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetBD1
+{
+    public partial class Detalhes : System.Web.UI.Page
+    {
+        #region Page_Load
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (CapturaID())
+            {
+                DadosConsulta();
+            }
+        }
+        #endregion
+
+        #region CapturaID
+        private bool CapturaID()
+        {
+            return Request.QueryString.AllKeys.Contains("id");
+        }
+        #endregion
+
+        #region DadosConsulta
+        private void DadosConsulta()
+        {
+            int IDCliente = ObterIDCliente();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao.Connection;
+                cmd.CommandText = @"select * from cliente
+                                    where cli_id = @IDCliente";
+
+                cmd.Parameters.AddWithValue("@IDCliente", IDCliente);
+
+                Conexao.Conectar();
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    txtID.Text = reader["cli_id"].ToString();
+                    txtNome.Text = reader["cli_nome"].ToString();
+                    txtLogradouro.Text = reader["cli_logradouro"].ToString();
+                    txtNumero.Text = reader["cli_numero"].ToString();
+                    txtComplemento.Text = reader["cli_complemento"].ToString();
+                    txtBairro.Text = reader["cli_bairro"].ToString();
+                    txtCidade.Text = reader["cli_cidade"].ToString();
+                    txtUF.Text = reader["cli_uf"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+        }
+        #endregion
+
+        #region ObterIDCliente
+        private int ObterIDCliente()
+        {
+            int id = 0;
+            var idURL = Request.QueryString["id"];
+            if(!int.TryParse(idURL, out id))
+            {
+                throw new Exception("ID Inválido");
+            }
+            if (id <= 0)
+            {
+                throw new Exception("ID Inválido");
+            }
+            return id;
+        }
+        #endregion
+    }
+}
