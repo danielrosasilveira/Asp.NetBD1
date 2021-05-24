0c7afd7077373ad9d634770335a1d8a87eaffe02
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2021-05-24 20:19:25 -0300

    Apagar Cliente

---
+++ Asp.NetBD1/Asp.NetBD1/Remover.aspx.cs
@@ -0,0 +1,105 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetBD1
+{
+    public partial class Remover : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (CapturaID())
+            {
+                DadosConsulta();
+            }
+        }
+
+        private bool CapturaID()
+        {
+            return Request.QueryString.AllKeys.Contains("id");
+        }
+
+        private void DadosConsulta()
+        {
+            int idCliente = ObterIDCliente();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao.Connection;
+                cmd.CommandText = @"select * 
+                                    from cliente 
+                                    where cli_id=@id";
+
+                cmd.Parameters.AddWithValue("@id", idCliente);
+
+                Conexao.Conectar();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    txtID.Text = reader["cli_id"].ToString();
+                    txtNome.Text = reader["cli_nome"].ToString();
+                    txtLogradouro.Text = reader["cli_logradouro"].ToString();
+                    txtNumero.Text = reader["cli_numero"].ToString();
+                    txtComplemento.Text = reader["cli_complemento"].ToString();
+                    txtBairro.Text = reader["cli_bairro"].ToString();
+                    txtCidade.Text = reader["cli_cidade"].ToString();
+                    txtUF.Text = reader["cli_uf"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+        }
+
+        private int ObterIDCliente()
+        {
+            var id = 0;
+            var idURL = Request.QueryString["id"];
+            if (!int.TryParse(idURL, out id))
+            {
+                throw new Exception("ID Inválido");
+            }
+            if (id <= 0)
+            {
+                throw new Exception("ID Inválido");
+            }
+            return id;
+        }
+
+        protected void btnRemover_Click(object sender, EventArgs e)
+        {
+            int idCliente = ObterIDCliente();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao.Connection;
+                cmd.CommandText = @"delete from cliente
+                                    where cli_id = @id";
+
+                cmd.Parameters.AddWithValue("@id", idCliente);
+
+                Conexao.Conectar();
+                cmd.ExecuteNonQuery();
+
+                Response.Redirect("listar.aspx");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                Conexao.Desconectar();
+            }
+        }
+    }
+}
